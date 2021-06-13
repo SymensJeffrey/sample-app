146e27990bac5a00142b61ee5357c8a9a64e19d5
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-13 10:16:53 -0500

    add 3 paths

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,2 +1,13 @@
 class ExamplePagesController < ApplicationController
+  def first_action
+    render json: {message: "First message"}
+  end
+
+  def second_action
+    render json: {message: "WAZZZ UPPPPPP"}
+  end
+
+  def third_action
+    render json: {array_1: [3, 5, 19, 0]}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/first_path", controller: "first_controller", action: "first_action"
+  get "/first_path", controller: "example_pages", action: "first_action"
+  get "/second_path", controller: "example_pages", action: "second_action"
+  get "/third_path", controller: "example_pages", action: "third_action"
 end
